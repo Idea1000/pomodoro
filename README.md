ce8ecc9adc0d4ef571583588e71ff2f6a79e1291
brault231 <brault231@etu.unicaen.fr>
2024-09-26 15:41:40 +0200

    cycle fonctionnel

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pomodoro
+## Exercice JavaScript demandant de réaliser un timer avec la fameuse technique du "Pomodoro"
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,39 @@
-const TIMER = 60;
+const TIMERTRAVAIL = 3; //1500
+const TIMERPAUSE = 2; //300
+const CYCLE = 2;
+
+function changement() {
+    if (state % 2 == 1) {
+        //travail
+        time = TIMERTRAVAIL;
+        usedTimer = TIMERTRAVAIL;
+        document.getElementById("travail").style.color = "Yellow";
+        document.getElementById("pause").style.color = "White";
+        state++;
+    } else if (state % 2 == 0 && state != (CYCLE * 2)) {
+        //pause
+        time = TIMERPAUSE;
+        usedTimer = TIMERPAUSE;
+        document.getElementById("travail").style.color = "White";
+        document.getElementById("pause").style.color = "Yellow";
+        state++;
+    } else {
+        //grande pause
+        time = TIMERTRAVAIL;
+        usedTimer = TIMERTRAVAIL;
+        document.getElementById("travail").style.color = "White";
+        document.getElementById("pause").style.color = "Yellow";
+        state=1;
+    }
+}
 
 /**
  * enleve 1 au timer et supprime l'intervalle si il arrive à 0
  */
 function reduceTime() {
     if (time <= 0) {
-        clearInterval(reduce);
+        /*clearInterval(reduce);*/
+        changement();
     } else {
         time -= 1;
     }
@@ -23,7 +51,7 @@ function updateTimer(duree, display) {
         second = "0"+second;
     }
     display.innerText = minute + ":" + second;
-    bar.animate((duree/TIMER)*1.0);
+    bar.animate((duree/usedTimer)*1.0);
 }
 
 /**
@@ -33,11 +61,14 @@ function handleStartStop() {
     if (allume) {
         //eteindre
         clearInterval(reduce);
-        time = TIMER;
+        state = 1;
+        changement();
+        state = 1;
         updateTimer(time, affichage);
-        if ( document.getElementById("ss").classList.contains('fa-rotate') ) {
-            document.getElementById("ss").classList.remove('fa-rotate');
-            document.getElementById("ss").classList.add('fa-play');
+        let icon = document.getElementById("ss");
+        if ( icon.classList.contains('fa-rotate') ) {
+            icon.classList.remove('fa-rotate');
+            icon.classList.add('fa-play');
          }
         allume = false;
         bar.animate(1);
@@ -47,10 +78,10 @@ function handleStartStop() {
             reduceTime();
             updateTimer(time, affichage);
         }, 1000);
-        
-        if ( document.getElementById("ss").classList.contains('fa-play') ) {
-           document.getElementById("ss").classList.remove('fa-play');
-           document.getElementById("ss").classList.add('fa-rotate');
+        let icon = document.getElementById("ss");
+        if ( icon.classList.contains('fa-play') ) {
+            icon.classList.remove('fa-play');
+            icon.classList.add('fa-rotate');
         }
         allume = true;
     }
@@ -67,9 +98,12 @@ let bar = new ProgressBar.Circle("#progressionCircle", {
   });
 bar.set(1);
 
+let state = 1; //important
+let usedTimer;
 let allume = false;
 document.getElementById("play").addEventListener("click", handleStartStop);
 let affichage = document.getElementById("timer");
-let time = TIMER;
+let time;
+changement();
 updateTimer(time, affichage);
 let reduce;
